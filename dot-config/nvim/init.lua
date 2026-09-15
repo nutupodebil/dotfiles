6595c3ca558f3e26e3f7f637474384b97bdb2208
nutupodebil <vladimirzh09@gmail.com>
2026-09-16 00:29:36 +0300

    Major Cleanup

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+--vim.opt.termguicolors = true
+
 --vim.cmd("colorscheme matugen")
 --vim.api.nvim_create_autocmd("Signal", {
 --    pattern = "SIGUSR1",
